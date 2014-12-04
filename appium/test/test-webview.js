6ffd8cd5b837037192b6dc530ea54535e8335ee3
Nolan Lawson <nlawson@squarespace.com>
2014-12-04 11:41:39 -0500

    repro chromedriver error instead

--- appium/test/test-webview.js
+++ appium/test/test-webview.js
@@ -5,34 +5,23 @@ var packageName = 'webview.com.example.nlawson.webview';
 function test(browser, driver) {
   return browser
     .sleep(5000)
-    .elementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]" +
-      "/android.widget.RelativeLayout[1]/android.view.View[1]/android.view.View[3]" + 
-      "/android.widget.EditText[1]").sendKeys("entering some text")
-    .sleep(10000)
-    .elementByXPath('//android.webkit.WebView')
-    /*
-    .click()
-    .sleep(1000)
     .contexts()
     .then(function (ctxs) {
       console.log(ctxs);
       var newContext = ctxs.filter(function (ctx) {
-         return /WEBVIEW.*com.example.nlawson/.test(ctx);
+         return /WEBVIEW.*nlawson/.test(ctx);
       })[0];
       return driver.context(newContext);
     })
-    .sleep(1000)
-    .elementByCss('.my-text-area')
-    .click()
-    .sendKeys('This is the text I have successfully typed')
-    .sleep(1000)
+    .elementByCssSelector('.my-text-area')
+    .sendKeys('yo I am totally entering some text')
+    .sleep(5000)
     .contexts()
     .then(function (ctxs) {
       return ctxs.filter(function (ctx) {
         return ctx.indexOf('NATIVE') !== -1;
       })[0];
-    }).sleep(1000)
-    */
+    })
     ;
 }
 
