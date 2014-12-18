2657d25f677894f9e0e8e47edcd9fcfebc82c5de
Nolan Lawson <nlawson@squarespace.com>
2014-12-18 10:38:54 -0500

    simplify

--- app/src/main/java/webview/com/example/nlawson/webview/WebViewFragment.java
+++ app/src/main/java/webview/com/example/nlawson/webview/WebViewFragment.java
@@ -25,68 +25,11 @@ public class WebViewFragment extends Fragment {
     final WebView webView = (WebView) rootView.findViewById(R.id.web_view);
     WebSettings settings = webView.getSettings();
     settings.setJavaScriptEnabled(true);
-    settings.setDatabaseEnabled(true);
-    settings.setAllowFileAccess(true);
-    settings.setDomStorageEnabled(true);
-    settings.setDefaultTextEncodingName("utf-8");
-    settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-    webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-    settings.setAllowUniversalAccessFromFileURLs(true);
 
-    //Pipe javascript console messages through to logcat
-    webView.setWebChromeClient(new WebChromeClient() {
-      @Override
-      public boolean onConsoleMessage(ConsoleMessage cm) {
-        Log.d("Console", cm.message() + " -- From line " + cm.lineNumber() + " of " + cm.sourceId());
-        return true;
-      }
-    });
-
-    webView.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-      WebView.setWebContentsDebuggingEnabled(true); // for appium
-    }
-
-    new AsyncTask<Void, Void, Void>(){
-
-      @Override
-      protected Void doInBackground(Void... voids) {
-        try {
-          Thread.sleep(1000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
-        webView.loadUrl("file:///android_asset/index.html");
-      }
-    }.execute((Void)null);
+    webView.loadUrl("file:///android_asset/index.html");
 
     return rootView;
 
   }
 
-  public class MyJavaScriptInterface {
-
-    @JavascriptInterface
-    public String doSomething() {
-      return "yo";
-    }
-
-    @JavascriptInterface
-    public String doSomethingElse() {
-      return "hey";
-    }
-
-    @JavascriptInterface
-    public void sendRequest(String str) {
-      Log.d("yo", str);
-    }
-
-  }
 }
